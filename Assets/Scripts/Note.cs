5398596ee39128179b53fe393d6f8f1141eff0e5
Andrew <adiep@dons.usfca.edu>
2023-10-14 00:14:25 -0700

    Modify direction choice

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -28,19 +28,19 @@ public class Note : MonoBehaviour
         {
             if (lane.direction == 0) {
                 print("left");
-                transform.localPosition = Vector2.Lerp(Vector2.left * lane.noteSpawnX, Vector2.left * lane.noteDespawnX, t);
+                transform.localPosition = Vector2.Lerp(Vector2.right * lane.noteSpawnX, Vector2.right * lane.noteDespawnX, t);
             }
             else if (lane.direction == 1) {
                 print("up");
-                transform.localPosition = Vector2.Lerp(Vector2.up * lane.noteSpawnY, Vector2.up * lane.noteDespawnY, t);
+                transform.localPosition = Vector2.Lerp(Vector2.down * lane.noteSpawnY, Vector2.down * lane.noteDespawnY, t);
             }
             else if (lane.direction == 2) {
                 print("down");
-                transform.localPosition = Vector2.Lerp(Vector2.down * lane.noteSpawnY, Vector2.down * lane.noteDespawnY, t);
+                transform.localPosition = Vector2.Lerp(Vector2.up * lane.noteSpawnY, Vector2.up * lane.noteDespawnY, t);
             }
             else if (lane.direction == 3) {
                 print("right");
-                transform.localPosition = Vector2.Lerp(Vector2.right * lane.noteSpawnX, Vector2.right * lane.noteDespawnX, t);
+                transform.localPosition = Vector2.Lerp(Vector2.left * lane.noteSpawnX, Vector2.left * lane.noteDespawnX, t);
             }
             GetComponent<SpriteRenderer>().enabled = true;
         }
