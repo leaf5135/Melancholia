d96809b4c47ea7ddb5b3eeb66d497868490e91b6
Andrew <adiep@dons.usfca.edu>
2023-11-03 18:16:11 -0700

    Implement combo, score, health (backend)

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,29 +10,41 @@ public class ScoreManager : MonoBehaviour
     public AudioSource inaccuratehitSFX;
     public AudioSource missSFX;
     public TMPro.TextMeshPro scoreText;
-    static int comboScore;
+    static int combo;
+    static int score;
+    static int health;
     void Start()
     {
         Instance = this;
-        comboScore = 0;
+        combo = 0;
+        score = 0;
+        health = 25;
     }
     public static void Hit()
     {
-        comboScore += 1;
+        combo += 1;
+        score += 1;
         Instance.hitSFX.Play();
+        System.Random rnd = new System.Random();
+        int regen  = rnd.Next(1, 25);
+        if (regen < 5) {
+            health += 1;
+        }
     }
     public static void InaccurateHit() {
         Instance.inaccuratehitSFX.Play();
     }
     public static void Miss()
     {
-        if (comboScore > 0) {
-            comboScore -= 1;
+        combo = 0;
+        if (score > 0) {
+            score -= 1;
         }
+        health -= 4;
         Instance.missSFX.Play();
     }
     private void Update()
     {
-        scoreText.text = comboScore.ToString();
+        scoreText.text = score.ToString();
     }
 }
