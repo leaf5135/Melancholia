424c8c6fa0f00ce5b2af6ccf74974e43c990b629
Andrew <adiep@dons.usfca.edu>
2024-07-22 23:06:42 -0700

    Remove health bar temporarily

--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class StatsManager : MonoBehaviour
 {
     public static StatsManager Instance;
-    public static HealthBar bar;
     public AudioSource hitSFX;
     public AudioSource inaccuratehitSFX;
     public AudioSource missSFX;
@@ -15,27 +14,14 @@ public class StatsManager : MonoBehaviour
     public TMPro.TextMeshPro comboText;
     static int combo;
     static int score;
-    public int maxHealth = 25;
-    public int health;
+    static int health;
+    private int maxHealth = 25;
     void Start()
     {
         Instance = this;
         combo = 0;
         score = 0;
         health = maxHealth;
-
-        // Find the HealthBar component in the scene
-        bar = FindObjectOfType<HealthBar>();
-
-        if (bar != null)
-        {
-            // Set the max health on the HealthBar
-            bar.SetMaxHealth(health);
-        }
-        else
-        {
-            Debug.LogError("HealthBar not found in the scene!");
-        }
     }
     public static void Hit()
     {
@@ -46,7 +32,6 @@ public class StatsManager : MonoBehaviour
         int regen  = rnd.Next(1, 25);
         if (regen < 5) {
             health += 1;
-            bar.SetHealth(health);
         }
     }
     public static void InaccurateHit() {
@@ -56,22 +41,17 @@ public class StatsManager : MonoBehaviour
         int dmg  = rnd.Next(1, 8);
         if (dmg <= 4) {
             health -= 1;
-            bar.SetHealth(health);
         }
     }
     public static void Miss()
     {
         combo = 0;
-        if (score > 0) {
-            score -= 1;
-        }
         health -= 4;
-        bar.SetHealth(health);
         Instance.missSFX.Play();
     }
     private void Update()
     {
-        scoreText.text = score.ToString();
-        comboText.text = combo.ToString();
+        scoreText.text = "Score - " + score.ToString();
+        comboText.text = "Combo - " + combo.ToString();
     }
 }
