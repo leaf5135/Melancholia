40428b97cd639a43c321760b8c33a402010b0162
Andrew <adiep@dons.usfca.edu>
2024-07-25 22:28:48 -0700

    Create HealthBar script

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private float maxHealth = 25;
+    private float currentHealth;
+    [SerializeField] private Image healthBarFill;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        updateHealthBar();
+    }
+
+    public float getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public float getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void updateHealth(float amount) {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        updateHealthBar();
+    }
+
+    private void updateHealthBar() {
+        float targetFillAmount = currentHealth / maxHealth;
+        healthBarFill.fillAmount = targetFillAmount;
+    }
+}
