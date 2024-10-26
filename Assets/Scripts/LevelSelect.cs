b8dec930832f0afcfb72abf534f2dae1c32be0b9
Andrew <adiep@dons.usfca.edu>
2024-10-26 02:45:05 -0700

    Refactor index access

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -6,53 +6,54 @@ using UnityEngine.SceneManagement;
 public class LevelSelect : MonoBehaviour
 {
 
-    public SpriteRenderer sr; 
+    public SpriteRenderer sr;
     public List<Sprite> albums = new List<Sprite>();
-    
     public List<AudioClip> clips = new List<AudioClip>();
     public AudioSource audioSource;
-    private int selectedSource = 0;
-
+    private int selectedScene = 0;
     private int selectedSong = 0;
     public GameObject songAlbum;
 
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            nextSong();
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            backSong();
+        }
+    }
+
     public void nextSong() {
-        selectedSong = selectedSong + 1;
-        if (selectedSong == albums.Count) {
-            selectedSong = 0;
-        }
+        print("album count: " + albums.Count);
+        print("clips count: " + clips.Count);
+
+        selectedSong = (selectedSong + 1) % albums.Count;
         sr.sprite = albums[selectedSong];
 
-        selectedSource = selectedSource + 1;
-        if (selectedSource == clips.Count) {
-            selectedSource = 0;
-        }
-        audioSource.clip = clips[selectedSource];
+        selectedScene = (selectedScene + 1) % clips.Count;
+        audioSource.clip = clips[selectedScene];
+
         Debug.Log($"Playing {audioSource.clip.name}");
         audioSource.Play();
     }
-    
 
-    public void BackOption() {
-        selectedSong = selectedSong - 1;
-        if (selectedSong < 0) {
-            selectedSong = albums.Count - 1;
-        }
+    public void backSong() {
+        print("album count: " + albums.Count);
+        print("clips count: " + clips.Count);
+
+        selectedSong = (selectedSong - 1 + albums.Count) % albums.Count;
         sr.sprite = albums[selectedSong];
 
-        selectedSource = selectedSource - 1;
-        if (selectedSource == clips.Count) {
-            selectedSource = 0;
-        }
-        audioSource.clip = clips[selectedSource];
+        selectedScene = (selectedScene - 1 + clips.Count) % clips.Count;
+        audioSource.clip = clips[selectedScene];
         Debug.Log($"Playing {audioSource.clip.name}");
         audioSource.Play();
     }
+
     // Start is called before the first frame update
     public void PlayGame(){
         Debug.Log(sr.sprite.name);
-        
         SceneManager.LoadScene(sr.sprite.name);
     }
 }
